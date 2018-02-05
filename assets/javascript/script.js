76cfce55fb3f1bd0ddb2e179d49bfd394a7f0b21
jheilhet <shirley.mramirez@yahoo.com>
2018-02-04 20:10:57 -0700

    added code .click() for numbers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,29 @@
-    window.onload = function() {
-        // your code here
-        document.getElementById("lets_go").innerHTML = "Lemme Go!";
+$(document).ready(function() {
+    var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length - 9, 9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
     };
+    var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
 
-    $(document).ready(function() {
-        $("#lets_go").text("Lemme Go!");
+    //Add a single click listener to all of the #numbers a, except for #clear or #clearall that:
+    $("#numbers > a").not("#clear,#clearall").click(function() {
+
+        //Takes the .html() of the number button and appends that to number.
+        number += $(this).text();
+
+        //Sets the .text() of totaldiv to number.
+        totaldiv.text(number);
+
+        // Call testNumLength, passing in number as the parameter.
+        testNumLength(number);
     });
+});
