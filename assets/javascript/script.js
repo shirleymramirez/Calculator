9f33651d79d48afb2a4bf1a55e7da1f06fb8bdae
jheilhet <shirley.mramirez@yahoo.com>
2018-02-04 18:40:09 -0700

    added basic html code for calculator

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+    window.onload = function() {
+        // your code here
+        document.getElementById("lets_go").innerHTML = "Lemme Go!";
+    };
+
+    $(document).ready(function() {
+        $("#lets_go").text("Lemme Go!");
+    });
